8e348273983829f475c0c6473c07b0a0cbcead14
Chr1stopher-H <88154155+Chr1stopher-H@users.noreply.github.com>
2021-07-29 19:35:54 -0400

    Master will now display time until an action will be preformed

--- timetesting.py
+++ timetesting.py
@@ -1,6 +1,23 @@
 import time
-
-if spawntime == False:
-	spawnseed = randint(240, 480)
-	spawnstarttime = time.time()
-	spawntime = True
+from random import randint
+spawntime = False
+while True:
+	if spawntime == False:
+		spawnseed = randint(5, 10)
+		spawnstarttime = time.time()
+		spawntime = True
+	if time.time() - spawnstarttime >= spawnseed:
+		print("Setting Spawn!")
+		spawnstarttime = time.time()
+		mouse.release(Button.left)
+		for char in "/setspawn":
+			keyboard.press(char)
+			keyboard.release(char)
+			time.sleep(random.random() / 4)
+			keyboard.press(Key.enter)
+			keyboard.release(Key.enter)
+			spawnseed = randint(240,480)
+			print("spawnseed is " + str(spawnseed))
+			mouse.press(Button.left)
+	timeleft = int(spawnseed - (time.time() - spawnstarttime))
+	print(str(timeleft) + " seconds left!")

d76704f908b44f136ee2e4b2e5d31866fb328afe
Chr1stopher-H <88154155+Chr1stopher-H@users.noreply.github.com>
2021-07-29 21:07:09 -0400

    failing to remove all of a color

--- cvtest.py
+++ cvtest.py
@@ -1,15 +1,21 @@
-import cv2
-import numpy
-import matplotlib
+import cv2 as cv2
+import numpy as np
 
+image = cv2.imread(r"C:\Users\choll\AppData\Roaming\.minecraft\screenshots\rmsky.png")
+hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 
-path = r'C:\Users\choll\AppData\Roaming\.minecraft\screenshots\photo.png'
-img = cv2.imread(path)
+# Define lower and uppper limits of what we call "brown"
+brown_lo=np.array([185, 210, 250])
+brown_hi=np.array([195, 220, 255])
 
-window_name = 'photo'
+# Mask image to only select browns
+mask=cv2.inRange(hsv,brown_lo,brown_hi)
 
-cv2.imshow(window_name, img)
+# Change image to red where we found brown
+image[mask>0]=(0,0,255)
 
-cv2.waitKey(0)
+cv2.imwrite("result.png",image)
 
-cv2.destroyAllWindows()
+cv2.imwrite("result.png",image)
+cv2.imshow("Original", image)
+cv2.waitKey(10000)

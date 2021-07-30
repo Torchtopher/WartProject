a73b332ca684c6647a734c3e83ed5d395466d562
Chr1stopher-H <88154155+Chr1stopher-H@users.noreply.github.com>
2021-07-30 11:15:59 -0400

    Fixing jumping issues

--- keytest.py
+++ keytest.py
@@ -1,5 +1,25 @@
-
+import time
 from pynput.keyboard import Key, Controller
+import random
 keyboard = Controller()
-
-keyboard.press("p")
+time.sleep(2)
+while True:
+    print("Jumping!!")
+    keyboard.press(Key.space)
+    time.sleep(0.1)
+    keyboard.release(Key.space)
+    time.sleep(0.05)
+    keyboard.press(Key.space)
+    time.sleep(0.1)
+    time.sleep(0.1)
+    keyboard.release(Key.space)
+    jumpdelay = random.random() * 3 + .2
+    print("jumpdelay is " + str(jumpdelay))
+    time.sleep(jumpdelay)
+    keyboard.press(Key.space)
+    time.sleep(0.1)
+    keyboard.release(Key.space)
+    time.sleep(0.1)
+    keyboard.press(Key.space)
+    time.sleep(0.1)
+    keyboard.release(Key.space)

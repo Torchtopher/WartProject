4a49439e5cce14fbcaa66d3f060e380008751831
Chr1stopher-H <88154155+Chr1stopher-H@users.noreply.github.com>
2021-07-30 11:36:22 -0400

    Turn sky to black to hopefully fully remove blue false positives

--- cvtest.py
+++ cvtest.py
@@ -5,17 +5,16 @@ image = cv2.imread(r"C:\Users\choll\AppData\Roaming\.minecraft\screenshots\rmsky
 hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 
 # Define lower and uppper limits of what we call "brown"
-brown_lo=np.array([185, 210, 250])
-brown_hi=np.array([195, 220, 255])
-
+brown_lo=np.array([100, 20, 20])
+brown_hi=np.array([110, 255, 255])
 # Mask image to only select browns
 mask=cv2.inRange(hsv,brown_lo,brown_hi)
 
 # Change image to red where we found brown
-image[mask>0]=(0,0,255)
+image[mask>0]=(0,0,0)
 
 cv2.imwrite("result.png",image)
 
 cv2.imwrite("result.png",image)
-cv2.imshow("Original", image)
+cv2.imshow("Result", image)
 cv2.waitKey(10000)
